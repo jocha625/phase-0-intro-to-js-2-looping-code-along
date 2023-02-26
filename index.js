a8bdc20bafdfc2454b0916e5043238ce32c8c1eb
Jorge Chavez Meza <chavezmezajorge0@gmail.com>
2023-02-26 17:04:58 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+const names=["Guadalupe","Ollie","Aki"]
+function writeCards(names, event) {
+    let completedCards =[]
+    for (let i=0;i < names.length; i++){
+        
+        completedCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    console.log(completedCards);
+   
+  
+    }
+    return completedCards; 
+}
+
+writeCards(names,"Suprise")
+
+
+function countDown(positiveNumber) {
+    while (positiveNumber >= 0) {
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+}
